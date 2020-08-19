3bde74970b1165fde7c9be78d72a8cbf4ecc6aac
Nafiur Rashid <nafiurrashid@gmail.com>
2020-08-19 15:58:46 +0600

    jsonModel

---
+++ Cool.Website/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Cool.Website.Models
+{
+    public class Product
+    {
+        public string date { get; set; }
+        public string trade_codet { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string open { get; set; }
+        public string close { get; set; }
+        public string volume { get; set; }
+
+        public override string ToString()=>JsonSerializer.Serialize<Product>(this);
+
+      
+
+
+
+    }   
+}
---
+++ Cool.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+using System.IO;
+
+using System.Text.Json;
+
+using Microsoft.AspNetCore.Hosting;
+using Cool.Website.Models;
+
+namespace Cool.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "stock_market_data.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
