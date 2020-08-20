4e327b168c0f2a301e618f777647bd5a08fcc94b
Nafiur Rashid <nafiurrashid@gmail.com>
2020-08-21 00:47:49 +0600

    sqlModel

---
+++ BlazorApp/BlazorApp/Models/MarketModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class MarketModel
+    {
+        public string trade_code { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string open { get; set; }
+        public string close { get; set; }
+        public string volume { get; set; }
+
+
+
+    }
+}
---
+++ BlazorApp/Data library/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Data_library
+{
+    public interface IDataAccess
+    {
+        
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
+        Task SaveData<T>(string sql, T parameters, string connectionString);
+    }
+}
